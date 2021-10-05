dac4f33b6053f27b539bb7d0b3d4c55aef64c741
Scott Provence <scott.provence@yahoo.com>
2021-10-05 16:29:53 -0400

    Implement accordion component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import JumbotronContainer from "./containers/jumbotron";
 import FooterContainer from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
