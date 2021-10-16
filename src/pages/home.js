3c81ee0829c916ccda9b349f593b4e4a13fec1f4
Scott Provence <scott.provence@yahoo.com>
2021-10-16 17:53:19 -0400

    Implement Header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,32 @@
+import OptForm from "../components/opt-form";
+import Feature from "../components/feature";
 import JumbotronContainer from "../containers/jumbotron";
 import FooterContainer from "../containers/footer";
 import FaqsContainer from "../containers/faqs";
+import HeaderContainer from "../containers/header";
 
 const Home = () => {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
